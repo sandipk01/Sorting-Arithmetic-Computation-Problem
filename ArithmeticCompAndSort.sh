95ee23b2b80714df9d63eca467aad0aeb7d548a1
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-26 10:05:26 +0530

    REFACTORED : Performed operation on multiplication and additon

--- ArithmeticCompAndSort.sh
+++ ArithmeticCompAndSort.sh
@@ -4,6 +4,7 @@
 
 #VARIABLES
 operation1
+operation2
 
 #TAKING THREE INPUTS
 printf "Enter the value of a\n"
@@ -14,5 +15,7 @@ printf "Enter the value of c\n"
 read c
 
 operation1=$(( $a+$b*$c ))
+operation2=$(( $a*$b+$c ))
 
-printf "Operation1=$operation1"
+printf "Operation1=$operation1\n"
+printf "Operation2=$operation2\n"
