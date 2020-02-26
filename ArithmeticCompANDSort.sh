3652c7bdd12ee49dd5becc2f518d14fcb36b10b0
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-26 09:39:22 +0530

    ADDED : AirthmeticCompANDSort.sh and added Three inputs

---
+++ ArithmeticCompANDSort.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+#Write program to take three inputs – a, b & c
+
+#TAKING THREE INPUTS
+printf "Enter the value of a\n"
+read a
+printf "Enter the value of b\n"
+read b
+printf "Enter the value of c\n"
+read c
+
+printf "a=$a b=$b c=$c \n"
