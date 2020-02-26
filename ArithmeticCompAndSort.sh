c7facb5f75417452b92791ef446d001bda605211
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-26 10:28:29 +0530

    REFACTORED : Every operation is stored in dictionary

--- ArithmeticCompAndSort.sh
+++ ArithmeticCompAndSort.sh
@@ -3,6 +3,7 @@
 #Write program to take three inputs – a, b & c
 
 #VARIABLES
+declare -A operations
 operation1=0
 operation2=0
 operation3=0
@@ -16,12 +17,23 @@ read b
 printf "Enter the value of c\n"
 read c
 
+#ARITHMETIC OPERATIONS
 operation1=$(( $a+$b*$c ))
 operation2=$(( $a*$b+$c ))
 operation3=$(( $c+$a/$b ))
 operation4=$(( $a%$b+$c ))
 
+#PRINTING
 printf "Operation1=$operation1\n"
 printf "Operation2=$operation2\n"
 printf "Operation3=$operation3\n"
 printf "Operation4=$operation4\n"
+
+#STORING OPERATIONS IN DICTIONARY
+operations[0]=$operation1
+operations[1]=$operation2
+operations[2]=$operation3
+operations[3]=$operation4
+
+echo ${!operations[@]}
+
