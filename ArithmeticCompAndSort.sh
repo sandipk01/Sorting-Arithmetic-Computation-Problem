0fd87e9b1404ad11d19c2a97bb08fac09d824e08
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-26 10:16:24 +0530

    REFACTORED : Performed operation on modulus and addition

--- ArithmeticCompAndSort.sh
+++ ArithmeticCompAndSort.sh
@@ -3,9 +3,10 @@
 #Write program to take three inputs – a, b & c
 
 #VARIABLES
-operation1
-operation2
-operation3
+operation1=0
+operation2=0
+operation3=0
+operation4=0
 
 #TAKING THREE INPUTS
 printf "Enter the value of a\n"
@@ -18,8 +19,9 @@ read c
 operation1=$(( $a+$b*$c ))
 operation2=$(( $a*$b+$c ))
 operation3=$(( $c+$a/$b ))
+operation4=$(( $a%$b+$c ))
 
 printf "Operation1=$operation1\n"
 printf "Operation2=$operation2\n"
 printf "Operation3=$operation3\n"
-
+printf "Operation4=$operation4\n"
